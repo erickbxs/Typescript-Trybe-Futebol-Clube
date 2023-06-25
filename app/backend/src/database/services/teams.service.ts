12abaa9641481a5f5e4b41e3a6b98148372d14f7
erickbxs <erickbxs@mgmail.com>
2023-06-25 14:46:32 -0300

    att 05

--- app/backend/src/database/services/teams.service.ts
+++ app/backend/src/database/services/teams.service.ts
@@ -4,26 +4,24 @@ class TeamService {
   constructor(private teamsModel: typeof TeamsModel = TeamsModel) {}
 
   public async getAllTeams(): Promise<{
-    success: boolean;
-    message: string;
     data: TeamsModel[] | null;
   }> {
     try {
       const teams = await this.teamsModel.findAll();
-      return { success: true, message: 'Success', data: teams || null };
+      return { data: teams || null };
     } catch (error) {
-      return { success: false, message: 'Failed to fetch teams', data: null };
+      return { data: null };
     }
   }
 
   public async getOneTeam(
     id: number,
-  ): Promise<{ success: boolean; message: string; data: TeamsModel | null }> {
+  ): Promise<{ data: TeamsModel | null }> {
     try {
       const team = await this.teamsModel.findOne({ where: { id } });
-      return { success: true, message: 'Success', data: team || null };
+      return { data: team || null };
     } catch (error) {
-      return { success: false, message: 'Failed to fetch team', data: null };
+      return { data: null };
     }
   }
 }
