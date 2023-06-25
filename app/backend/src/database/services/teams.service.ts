adba0cec65825a384a0945e01938eae5ea5175a1
erickbxs <erickbxs@mgmail.com>
2023-06-25 15:04:48 -0300

    att 08

--- app/backend/src/database/services/teams.service.ts
+++ app/backend/src/database/services/teams.service.ts
@@ -3,25 +3,19 @@ import TeamsModel from '../models/TeamsModel';
 class TeamService {
   constructor(private teamsModel: typeof TeamsModel = TeamsModel) {}
 
-  public async getAllTeams(): Promise<{
-    data: TeamsModel[] | null;
-  }> {
+  public async getAllTeams(): Promise<TeamsModel[] | null> {
     try {
-      const teams = await this.teamsModel.findAll();
-      return { data: teams || null };
+      return await this.teamsModel.findAll();
     } catch (error) {
-      return { data: null };
+      return null;
     }
   }
 
-  public async getOneTeam(
-    id: number,
-  ): Promise<{ data: TeamsModel | null }> {
+  public async getOneTeam(id: number): Promise<TeamsModel | null> {
     try {
-      const team = await this.teamsModel.findOne({ where: { id } });
-      return { data: team || null };
+      return await this.teamsModel.findOne({ where: { id } });
     } catch (error) {
-      return { data: null };
+      return null;
     }
   }
 }
