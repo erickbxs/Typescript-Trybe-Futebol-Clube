6e9d573d391ce4d4e2d63da5c058da95a712bfbd
erickbxs <erickbxs@mgmail.com>
2023-06-27 12:47:20 -0300

    homeboard

--- app/backend/src/database/routes/homeBoard.router.ts
+++ app/backend/src/database/routes/homeBoard.router.ts
@@ -3,12 +3,8 @@ import HomeController from '../controllers/home.controller';
 
 const router = Router();
 
-const homeController = new HomeController();
+router.get('/home', (req, res) => new HomeController().getHomeBoard(req, res));
 
-router.get('/', (req, res) => homeController.getHomeBoard(req, res));
-
-router.get('/home', (req, res) => homeController.getHomeBoard(req, res));
-
-router.get('/away', (req, res) => homeController.getHomeBoard(req, res));
+router.get('/away', (req, res) => new HomeController().getHomeBoard(req, res));
 
 export default router;
